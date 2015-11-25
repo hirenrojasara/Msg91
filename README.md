b85ac9837f2f020ef84dc714e89fb56e528f900f
Hiren <hiren.soni@rapidops.com>
2015-11-25 11:36:18 +0530

    Initial API commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Msg91
 Msg91 API for node.js
+
+How to integrate:
+
+var msg91 = require("./app")("API_KEY", "SENDER_ID", "ROUTE_NO" );
+
+msg91.send("MOBILE_NO, "MESSAGE", function(err, response){
+    console.log(err);
+    console.log(response);
+})
+
+
+ROUTE_NO
+==============
+1 - Promotional Route
+4 - Transactional Route
---
+++ index.js
@@ -0,0 +1,67 @@
+/**
+ *
+ * @param authKey
+ * @param senderId
+ * @param route : Value can be 1 for Promotional Router or 4 for Transactional Route
+ */
+module.exports = function (authKey, senderId, route) {
+
+    if (authKey == null || authKey == "") {
+        throw new Error("MSG91 Authorization Key not provided.");
+    }
+
+    if (senderId == null || senderId == "") {
+        throw new Error("MSG91 Sender Id is not provided.");
+    }
+
+    if (route == null || route == "") {
+        throw new Error("MSG91 router Id is not provided.");
+    }
+
+    this.send = function (mobileNos, message, callback) {
+
+        if (mobileNos == null || mobileNos == "") {
+            throw new Error("MSG91 : Mobile No is not provided.");
+        }
+
+        if (message == null || message == "") {
+            throw new Error("MSG91 : message is not provided.");
+        }
+
+        var postData = "authkey=" + authKey + "&sender=" + senderId + "&mobiles=" + mobileNos + "&message=" + message + "&route=" + route;
+
+        var options = {
+            hostname: 'control.msg91.com',
+            port: 80,
+            path: '/api/sendhttp.php',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Content-Length': postData.length
+            }
+        };
+
+        var http = require("http");
+        var data = "";
+        var req = http.request(options, function (res) {
+            res.setEncoding('utf8');
+            res.on('data', function (chunk) {
+                data += chunk;
+            });
+            res.on('end', function () {
+                callback(null, data);
+            })
+        });
+
+        req.on('error', function (e) {
+            callback(e);
+        });
+
+        req.write(postData);
+        req.end();
+
+    };
+
+    return this;
+
+};
